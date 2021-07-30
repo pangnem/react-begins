4e9929fdd7eeb61c1fe1e1ce141fea2bb66c6b0a
Woonsan Ko <wsko@wise.co.kr>
2021-07-30 18:06:08 +0900

    lab01-post

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, React World!</h1>
+        <p>
+          시작이 반이다
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
