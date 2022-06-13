f90e59a8d00f9293d4bd8513b6d26b2e45058e78
pangnem <sprnd645@gmail.com>
2022-06-13 11:25:33 +0900

    style: ProductGrid

--- src/product-grid/ProductGrid.js
+++ src/product-grid/ProductGrid.js
@@ -1,62 +1,62 @@
 import './ProductGrid.css';
 import React from 'react';
-import { CategoryProductList } from './CategoryProductList';
+import {CategoryProductList} from './CategoryProductList';
 
 export class ProductGrid extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { error: null, loaded: false, categories: [] };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {error: null, loaded: false, categories: []};
+  }
 
-    componentDidMount() {
-        const categories = [];
-        fetch('data/products-data.json')
-            .then((res) => res.json())
-            .then(
-                (result) => {
-                    result.forEach((prod) => {
-                        const { category } = prod;
-                        if (!categories[category]) {
-                            categories[category] = [];
-                        }
-                        categories[category].push(prod);
-                    });
-                    this.setState({ error: null, loaded: true, categories });
-                },
-                (error) => {
-                    this.setState({ error, loaded: false, categories });
-                },
-            );
-    }
+  componentDidMount() {
+    const categories = [];
+    fetch('data/products-data.json')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          result.forEach((prod) => {
+            const {category} = prod;
+            if (!categories[category]) {
+              categories[category] = [];
+            }
+            categories[category].push(prod);
+          });
+          this.setState({error: null, loaded: true, categories});
+        },
+        (error) => {
+          this.setState({error, loaded: false, categories});
+        },
+      );
+  }
 
-    render() {
-        return(
-            <>
-                {this.state.error &&
-                    <div className="alert alert-danger" role="alert">
-                        Error occurred...
-                    </div>
-                }
-                {this.state.loaded &&
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Name</th>
-                                <th scope="col">Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {Object.keys(this.state.categories).map((category) =>
-                                <CategoryProductList
-                                    key={category}
-                                    category={category}
-                                    products={this.state.categories[category]} />
-                            )}
-                        </tbody>
-                    </table>
-                }
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        {this.state.error &&
+          <div className="alert alert-danger" role="alert">
+            Error occurred...
+          </div>
+        }
+        {this.state.loaded &&
+          <table className="table">
+            <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            {Object.keys(this.state.categories).map((category) =>
+              <CategoryProductList
+                key={category}
+                category={category}
+                products={this.state.categories[category]}/>
+            )}
+            </tbody>
+          </table>
+        }
+      </>
+    );
+  }
 }
