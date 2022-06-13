9fd9f02b9f869a8b364f003e43ccb7c5c7a8ae01
pangnem <sprnd645@gmail.com>
2022-06-13 10:42:03 +0900

    style: ProductGrid

--- src/product-grid/ProductGrid.js
+++ src/product-grid/ProductGrid.js
@@ -1,45 +1,45 @@
 import './ProductGrid.css';
 import React from 'react';
-import { productsData } from './products-data';
-import { CategoryProductList } from './CategoryProductList';
+import {productsData} from './products-data';
+import {CategoryProductList} from './CategoryProductList';
 
 export class ProductGrid extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = { categories: [] };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {categories: []};
+  }
 
-    componentDidMount() {
-        const categories = [];
-        productsData.forEach((prod) => {
-            const { category } = prod;
-            if (!categories[category]) {
-                categories[category] = [];
-            }
-            categories[category].push(prod);
-        });
-        this.setState({ categories });
-    }
+  componentDidMount() {
+    const categories = [];
+    productsData.forEach((prod) => {
+      const {category} = prod;
+      if (!categories[category]) {
+        categories[category] = [];
+      }
+      categories[category].push(prod);
+    });
+    this.setState({categories});
+  }
 
-    render() {
-        return (
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Name</th>
-                        <th scope="col">Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.keys(this.state.categories).map((category) =>
-                        <CategoryProductList
-                            key={category}
-                            category={category}
-                            products={this.state.categories[category]} />
-                    )}
-                </tbody>
-            </table>
-        );
-    }
+  render() {
+    return (
+      <table className="table">
+        <thead>
+        <tr>
+          <th scope="col">Name</th>
+          <th scope="col">Price</th>
+        </tr>
+        </thead>
+        <tbody>
+        {Object.keys(this.state.categories).map((category) =>
+          <CategoryProductList
+            key={category}
+            category={category}
+            products={this.state.categories[category]}/>
+        )}
+        </tbody>
+      </table>
+    );
+  }
 }
