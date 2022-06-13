28d214a7727596db5af2d92ffae2a61f11c4abfa
pangnem <sprnd645@gmail.com>
2022-06-13 11:06:03 +0900

    refactor: 상품 데이터 fetch로 요청

--- src/product-grid/ProductGrid.js
+++ src/product-grid/ProductGrid.js
@@ -12,34 +12,50 @@ export class ProductGrid extends React.Component {
 
   componentDidMount() {
     const categories = [];
-    productsData.forEach((prod) => {
-      const {category} = prod;
-      if (!categories[category]) {
-        categories[category] = [];
-      }
-      categories[category].push(prod);
-    });
-    this.setState({categories});
+    fetch('data/products-data.json')
+      .then((res) => res.json())
+      .then(result => {
+          result.forEach((prod) => {
+            const {category} = prod;
+            if (!categories[category]) {
+              categories[category] = [];
+            }
+            categories[category].push(prod);
+          });
+          this.setState({error: null, loaded: true, categories});
+        }, (error) => {
+          this.setState({error, loaded: false, categories});
+        },
+      );
   }
 
   render() {
-    return (
-      <table className="table">
-        <thead>
-        <tr>
-          <th scope="col">Name</th>
-          <th scope="col">Price</th>
-        </tr>
-        </thead>
-        <tbody>
-        {Object.keys(this.state.categories).map((category) =>
-          <CategoryProductList
-            key={category}
-            category={category}
-            products={this.state.categories[category]}/>
-        )}
-        </tbody>
-      </table>
+    return(
+      <>
+        {this.state.error &&
+          <div className="alert alert-danger" role="alert">
+            Error occurred...
+          </div>
+        }
+        {this.state.loaded &&
+          <table className="table">
+            <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            {Object.keys(this.state.categories).map((category) =>
+              <CategoryProductList
+                key={category}
+                category={category}
+                products={this.state.categories[category]} />
+            )}
+            </tbody>
+          </table>
+        }
+      </>
     );
   }
 }
